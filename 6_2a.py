ceb6c68d588a187702448fc5db3caa939d70668b
hucho <vonapets.n@gmail.com>
2020-03-13 01:51:56 +0200

    6_2a is done now

--- 6_2a.py
+++ 6_2a.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+import sys
 ip_addr_inp = input('Input ip address :')
 ip_addr = ip_addr_inp.split('.')
 for octet in ip_addr:
     if str(octet).isalpha():
         print('Ip address contains letters')
+        sys.exit()
     elif int(octet) < 0 or int(octet) > 255:
         print('Ip address contains out of range octets')
+        sys.exit()
 
 if 1 < int(ip_addr[0]) and int(ip_addr[0]) < 223:
     print("Unicast")
