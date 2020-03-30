8667e834c6585a3c6b495af9b7a08a314e478c8c
hucho <vonapets.n@gmail.com>
2020-03-30 23:43:48 +0300

    7_3b done

---
+++ 7_3b.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# script read data from cam_table.txt and print sorted data by vlan-number
+from sys import argv
+inp_file = argv[1]
+inp_vlan = int(argv[2])
+res_lst = []
+with open(inp_file, 'r') as inp_fl:
+    for line in inp_fl:
+        if 'DYNAMIC' in line: 
+            x = line.lstrip(' ').strip('\n').replace('DYNAMIC','')
+            res_lst.append(x.split(' '))
+
+for item in res_lst:
+    item[0] = int(item[0])
+res_lst.sort()
+for item in res_lst:
+    if inp_vlan in item:
+        if item[0] < 100:
+            print('{}    {}  {}'.format(item[0], item[5], item[-1]))
+        elif item[0] < 1000:
+            print('{}   {}  {}'.format(item[0], item[4], item[-1]))
+        elif item[0] > 999:
+            print('{}  {}  {}'.format(item[0], item[3], item[-1]))
