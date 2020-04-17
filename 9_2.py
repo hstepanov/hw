778826e0092d4aa16c1a8d9c8ee57e1bd5a4b9a7
hucho <vonapets.n@gmail.com>
2020-04-17 21:38:01 +0300

    9_2 in progress

---
+++ 9_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+trunk_mode_template = [
+    'switchport mode trunk', 'switchport trunk native vlan 999',
+    'switchport trunk allowed vlan'
+]
+
+trunk_config = {
+    'FastEthernet0/1': [10, 20, 30],
+    'FastEthernet0/2': [11, 30],
+    'FastEthernet0/4': [17]
+}
+for port, vlan in trunk_config.items():
+    print([vlan for vlan in trunk_config[port]])
+
+def conf_gen(trunk_mode_template, trunk_config):
+    for port, vlan in trunk_config.items():
+        print('interface', port)
+        for command in trunk_mode_template:
+            if command.endswith('vlan'):
+                print(command, [str(vl) for vl in vlan])
+            else:
+                print(command)
+conf_gen(trunk_mode_template, trunk_config)
