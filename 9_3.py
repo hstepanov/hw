61e27b328e4f01f787a32f94f8801eeede8c5b4a
hucho <vonapets.n@gmail.com>
2020-04-20 23:19:48 +0300

    9_3 done

--- 9_3.py
+++ 9_3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# script should return tuple of 2 dicts (access_dict and trunk_dict)
 from sys import argv
 input_file = argv[1]
 
@@ -9,13 +10,12 @@ def get_int_vlan_map(input_file):
         for line in inp_f:
             if 'Ethernet' in line:
                 interface = line.split()[-1]
-                print(interface)
             elif 'switchport access' in line:
                 access_vlan = line.split()[-1]
-                print(access_vlan)
+                access_dict[interface] = access_vlan
             elif 'switchport trunk allowed' in line:
-                trunk_vlan_list = line[31:]
-                print(trunk_vlan_list.rstrip().split(','))
-
-get_int_vlan_map(input_file)
+                trunk_vlan_list = line[31:].rstrip().split(',')
+                trunk_dict[interface] = trunk_vlan_list
+    return (access_dict, trunk_dict)
+print(get_int_vlan_map(input_file))
 
