48af7acf8ad126d558d50ca91bcd6038b9233730
hucho <vonapets.n@gmail.com>
2020-04-28 17:46:26 +0300

    9_4 in progress

--- 9_4.py
+++ 9_4.py
@@ -8,21 +8,25 @@ def convert_config_to_dict(config_name):
     result_dict = {}
 
     def ignore_command(command,ignore):
-        if command not in ignore:
+        if command in ignore:
             return True
         else:
             return False
 
     with open(config_name, 'r') as config:
         command_list = [line.rstrip() for line in config if '!' not in line]
-        print(command_list)
+        '''print(command_list)'''
         for command in command_list:
-            if not ignore_command(command,ignore) and not command.startswith(' '):
+            if ignore_command(command, ignore):
+                pass
+            else:
                 print(command)
                 result_dict[command] = []
+            '''if not ignore_command(command,ignore) and not command.startswith(' '):
+                print(command)'''
 
-    return print(result_dict)
+    return result_dict
 
 
 
-convert_config_to_dict(input_file)
+print(convert_config_to_dict(input_file))
