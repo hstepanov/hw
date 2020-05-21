01a8c71ebbcec2fcd3d6833f963baaaea5379393
hucho <vonapets.n@gmail.com>
2020-05-21 18:27:14 +0300

    in progress

--- 9_4.py
+++ 9_4.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#import pdb; pdb.set_trace()
 from sys import argv
 input_file = argv[1]
 
@@ -8,25 +9,28 @@ def convert_config_to_dict(config_name):
     result_dict = {}
 
     def ignore_command(command,ignore):
-        if command in ignore:
-            return True
-        else:
-            return False
+        ''' check if item in ignore list exists in config command line'''
+        for item in ignore: 
+            if item in command:
+                return True
+            else:
+                return False
 
     with open(config_name, 'r') as config:
         command_list = [line.rstrip() for line in config if '!' not in line]
-        '''print(command_list)'''
         for command in command_list:
             if ignore_command(command, ignore):
-                pass
-            else:
                 print(command)
+                #pass
+            else:
+                '''print(command)'''
                 result_dict[command] = []
             '''if not ignore_command(command,ignore) and not command.startswith(' '):
                 print(command)'''
 
     return result_dict
 
+    print('Current configuration' in ignore)
 
 
 print(convert_config_to_dict(input_file))
