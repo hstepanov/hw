035bf31c84454a5db018480291264142bccebba8
hucho <vonapets.n@gmail.com>
2020-05-23 18:10:43 +0300

    9_4 works in pythontutor, but won't in cmd :(

--- 9_4.py
+++ 9_4.py
@@ -3,39 +3,46 @@
 from sys import argv
 input_file = argv[1]
 
+
 def convert_config_to_dict(config_name):
     
     ignore = ['duplex', 'alias', 'Current configuration']
-    result_dict = {}
+    result_dct = {}
 
     def ignore_command(ignore, command):
         ''' check if item in ignore list exists in config command line'''
         for item in ignore: 
             if item in command:
                 return True
-#            else:
-#                return False
-    
 
     with open(config_name, 'r') as config:
         command_list = [line.rstrip() for line in config if '!' not in line]
+#command_list = [
+#    'interface Ethernet0/1',
+#    ' switchport trunk encapsulation dot1q',
+#    ' switchport trunk allowed vlan 100',
+#    ' switchport mode trunk',
+#    ' duplex auto',
+#    ' spanning-tree portfast edge trunk'
+#    ]
         cmd_index = 0
-        cmd_local_index = 0
+        cmd_index_local = 0
         for command in command_list:
             if ignore_command(ignore, command):
                 cmd_index += 1
                 pass
             elif not command.startswith(' '):
-                result_dict[command] = []
+                result_dct.setdefault(command, [])
                 cmd_index += 1
             elif command.startswith(' '):
-                result_dict.setdefault(command_list[cmd_index], []).append(command)
-                cmd_local_index += 1
-            else:
-                cmd_index = cmd_local_index + 1
+                """if string starts with whitespace,
+                then append it to the list of previous command"""
+    #                print(result_dct[command_list[cmd_index]])
+    #                print(result_dct[command_list[cmd_index - 1]])
+                result_dct[command_list[cmd_index - 1]].append(command)
+                cmd_index_local += 1
             
-    return result_dict
-
+    return result_dct
 
 
 print(convert_config_to_dict(input_file))
