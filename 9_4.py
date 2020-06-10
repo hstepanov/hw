e71483ed9dfeb3f4c1e3f84967789b97365b0640
hucho <vonapets.n@gmail.com>
2020-06-10 23:52:54 +0300

    9_4 is already done!

--- 9_4.py
+++ 9_4.py
@@ -17,32 +17,18 @@ def convert_config_to_dict(config_name):
 
     with open(config_name, 'r') as config:
         command_list = [line.rstrip() for line in config if '!' not in line]
-#command_list = [
-#    'interface Ethernet0/1',
-#    ' switchport trunk encapsulation dot1q',
-#    ' switchport trunk allowed vlan 100',
-#    ' switchport mode trunk',
-#    ' duplex auto',
-#    ' spanning-tree portfast edge trunk'
-#    ]
-        cmd_index = 0
-        cmd_index_local = 0
+
         for command in command_list:
             if ignore_command(ignore, command):
-                cmd_index += 1
                 pass
             elif not command.startswith(' '):
                 result_dct.setdefault(command, [])
-                cmd_index += 1
+#            if string starts with whitespace,
+#            then remove whitespace in the beginnning
+#            and append it to the list of previous command
             elif command.startswith(' '):
-                """if string starts with whitespace,
-                then append it to the list of previous command"""
-    #                print(result_dct[command_list[cmd_index]])
-    #                print(result_dct[command_list[cmd_index - 1]])
-                result_dct[command_list[cmd_index - 1]].append(command)
-                cmd_index_local += 1
-            
-    return result_dct
+                list(result_dct.values())[-1].append(command.lstrip())
 
+    return result_dct
 
 print(convert_config_to_dict(input_file))
